4a5a563113ec65be7bdb89640cae12490987122c
sheena849 <sheenamugo04@gmail.com>
2024-09-23 08:35:41 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const currentLeft = parseInt(dodger.style.left || '0', 10);
+  const newPosition = currentLeft - 10;
+
+  if (newPosition >= 0) {
+    dodger.style.left = `${newPosition}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const currentRight = parseInt(dodger.style.left || '0', 10);
+  const newPosition = currentRight + 10;
+
+  if (newPosition <= 360) {
+    dodger.style.left = `${newPosition}px`;
+  }
+}
